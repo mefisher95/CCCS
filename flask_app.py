c1811b22e7d28220fc261a9e950ea266cbc95f36
Michael Fisher <mefisher2@cougars.ccis.edu>
2022-01-17 14:21:34 -0600

    test changest to env

--- flask_app.py
+++ flask_app.py
@@ -2,24 +2,24 @@ from flask.templating import render_template
 from mysite import *
 from flask import Flask
 
-# from app.Menu_data import Menu_data
+# # from app.Menu_data import Menu_data
 
-def load_data():
-    import json
-    with open('mysite/static/site-data.json') as site_data:
-        return json.load(site_data)
+# def load_data():
+#     import json
+#     with open('mysite/static/site-data.json') as site_data:
+#         return json.load(site_data)
 
 
-@app.route("/")
-def index():
-    # data = Menu_data.Menu_data()
+# @app.route("/")
+# def index():
+#     # data = Menu_data.Menu_data()
 
-    menu_data = load_data()
-    print(menu_data)
+#     menu_data = load_data()
+#     print(menu_data)
 
-    return render_template('home.html', menu_data = menu_data)
+#     return render_template('home.html', menu_data = menu_data)
 
-# def main():
-#     in 
-#     if __name__ == "__main__":
-#         app.run()
+# # def main():
+# #     in 
+# #     if __name__ == "__main__":
+# #         app.run()
