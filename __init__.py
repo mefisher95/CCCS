004e5666db7adf549d93e724fc29fa81f5447226
Michael Fisher <mefisher2@cougars.ccis.edu>
2022-01-17 13:41:21 -0600

    test

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
-from app import *
+# from app import *
 
 
-from app import views
+# from app import views
 
 # menu_data = Menu_data()
 # print(menu_data)
