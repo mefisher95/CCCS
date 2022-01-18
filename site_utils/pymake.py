2aea8aafc6aadec1da19b75b95b1d49b377842ba
Michael Fisher <mefisher2@cougars.ccis.edu>
2022-01-17 19:20:15 -0600

    quick push

--- site_utils/pymake.py
+++ site_utils/pymake.py
@@ -2,7 +2,7 @@ from subprocess import run
 
 def make():
 
-    run('doxygen cccs_autodoc_file')
+    run('doxygen cccs_autodoc_file', shell=True)
     run('git add .', shell=True)
     run('git commit -m "quick push"', shell=True)
     run('git push', shell=True)
