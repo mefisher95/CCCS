1272e358b2717d3fcfafba0e5b0e256d4b7341fa
Michael Fisher <mefisher2@cougars.ccis.edu>
2022-01-18 17:14:23 -0600

    quick push

--- __init__.py
+++ __init__.py
@@ -1,12 +1,3 @@
-import imp
 from flask import Flask
 app = Flask(__name__)
 from mysite import views
-# from app import *
-
-
-# from app import views
-
-# menu_data = Menu_data()
-# print(menu_data)
-
