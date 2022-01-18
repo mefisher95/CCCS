6239a55e4661d0b3768573afec001419ba9e88db
Michael Fisher <mefisher2@cougars.ccis.edu>
2022-01-17 19:12:38 -0600

    quick push

---
+++ site_utils/__main__.py
@@ -0,0 +1,17 @@
+        
+# from Class import parse_class
+# from Function import parse_functions
+# from Dependencies import parse_dependencies, Module
+# from File import File
+# import os
+# import ast
+# import write_HTML
+
+import subprocess
+
+def main():
+    subprocess.run('git add .; git commit -m "quick push"; git push')
+
+
+if __name__ == "__main__":
+    main()
---
+++ site_utils/pymake.py
@@ -0,0 +1,5 @@
+import imp
+from subprocess import run
+
+
+run(['git add /mysite/.; git commit -m "quickpush"; git push'])
