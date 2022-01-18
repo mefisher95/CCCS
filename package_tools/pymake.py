8302d10b66a2481d991e1df8d6e8565e8eb1930f
Michael Fisher <mefisher2@cougars.ccis.edu>
2022-01-17 19:45:41 -0600

    quick push

--- package_tools/pymake.py
+++ package_tools/pymake.py
@@ -1,21 +1,18 @@
 from subprocess import run
 
-def make() -> None:
-    """
-    quick form packageing tool that runs the autodoc tool and pushes code to github
+"""
+quick form packageing tool that runs the autodoc tool and pushes code to github
 
-    executes:
-    doxygen cccs_autodoc_file
-    git add.
-    git commit -m "quick push"
-    git push
-    """
+executes:
+doxygen cccs_autodoc_file
+git add.
+git commit -m "quick push"
+git push
+"""
 
-    run(['doxygen', 'cccs_autodoc_file'])
-    # import os
-    # os.execl('doxygen', 'cccs_autodoc_file')
-    run('git add .', shell=True)
-    run('git commit -m "quick push"', shell=True)
-    run('git push', shell=True)
-
-make()
+run(['doxygen', 'cccs_autodoc_file'])
+# import os
+# os.execl('doxygen', 'cccs_autodoc_file')
+run('git add .', shell=True)
+run('git commit -m "quick push"', shell=True)
+run('git push', shell=True)
