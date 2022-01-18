9f4087940b80d33622fe5e4c916d76f6d12fc8bf
Michael Fisher <mefisher2@cougars.ccis.edu>
2022-01-17 19:13:31 -0600

    quick push

--- site_utils/__main__.py
+++ site_utils/__main__.py
@@ -10,7 +10,10 @@
 import subprocess
 
 def main():
-    subprocess.run('git add .; git commit -m "quick push"; git push')
+    subprocess.run('git add .', shell=True)
+    subprocess.run('git commit -m "quick push"', shell=True)
+    subprocess.run('git push', shell=True)
+
 
 
 if __name__ == "__main__":
