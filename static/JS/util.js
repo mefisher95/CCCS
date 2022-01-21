065381e69d16c726f5770c7117dfd84c641d9f14
Michael Fisher <mefisher2@cougars.ccis.edu>
2022-01-20 22:05:15 -0600

    quick push

--- static/JS/util.js
+++ static/JS/util.js
@@ -1,9 +1,9 @@
 function parse (s) { return JSON.parse(s); }
 
-$(function () {
-    $("#datepicker").datepicker({ 
-          autoclose: true, 
-          todayHighlight: true
-    }).datepicker('update', new Date());
-  });
+// $(function () {
+//     $("#datepicker").datepicker({ 
+//           autoclose: true, 
+//           todayHighlight: true
+//     }).datepicker('update', new Date());
+//   });
   
