c72d603cc1e2c5f6a88b05e29b0772c93efc5e69
Michael Fisher <mefisher2@cougars.ccis.edu>
2022-02-01 15:34:06 -0600

    testing the use of flask_session serverside Sessions

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_session import Session
+from mysite.config import *
 
 # from mysite.config import config_all, flask_config, database_config
 
-from mysite.config import *
 
 app = Flask(__name__)
 
@@ -15,14 +15,15 @@ app.config.from_object(config.__name__)
 
 app.secret_key = config.SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+Session(app)
 db = SQLAlchemy(app)
 
 from mysite import Database
+
 database_conn = Database.Database_handler()
 
 # login_manager.init_app(app)
 
 ### import last
 # from mysite.pages import index
-from mysite import pages, config
-
+from mysite import config, pages
